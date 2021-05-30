fe714071e108897fb1bd40d9688b71edfb562a96
Paul Matencio <paulo.matencio@gmail.com>
2021-05-30 22:14:06 +0200

    remove Write message

--- lib/utilities.go
+++ lib/utilities.go
@@ -2,10 +2,8 @@ package lib
 
 import (
 	"encoding/binary"
-	"fmt"
 	"io"
 )
-
 //  write msg tofile
 func Write(w io.Writer, msg []byte) error {
 
@@ -13,15 +11,11 @@ func Write(w io.Writer, msg []byte) error {
 		buf = make([]byte, 4)
 		err error
 	)
-
 	binary.LittleEndian.PutUint32(buf, uint32(len(msg)))
-
-	fmt.Println("Write: Size of the file",uint32(len(msg)))
-
+	//fmt.Println("Write: Size of the file",uint32(len(msg)))
 	if _, err = w.Write(buf); err != nil {
 		return err
 	}
-
 	if _, err = w.Write(msg); err != nil {
 		return err
 	} else {
@@ -31,18 +25,16 @@ func Write(w io.Writer, msg []byte) error {
 
 //  read msg from a file
 func Read(r io.Reader) ([]byte, error) {
-
 	var (
 		buf = make([]byte, 4)
 		err error
 	)
-
 	if _, err = io.ReadFull(r, buf); err != nil {
 		return nil, err
 	}
 
 	size := binary.LittleEndian.Uint32(buf)
-	fmt.Println("Reading: size of the file",size)
+	// fmt.Println("Reading: size of the file",size)
 	msg := make([]byte, size)
 
 	if _, err = io.ReadFull(r, msg); err != nil {
