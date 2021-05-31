aaa347c1d39c429b319729aef7ab665eeb4141ee
Paul Matencio <paulo.matencio@gmail.com>
2021-06-01 00:14:42 +0200

    add Number of pages

--- lib/createDocument.go
+++ lib/createDocument.go
@@ -3,10 +3,12 @@ package lib
 import "github.com/paulmatencio/protobuf-doc/src/document/documentpb"
 
 func CreateDocument(pn string, metadata string, pages int, object *[]byte )  *documentpb.Document {
+	// if big document, pages < 0 since it is initialized at the beginning
 	doc := &documentpb.Document{
 		DocId: pn,
 		Metadata: metadata,
-		NumberOfPages: int32(pages),
+		// NumberOfPages: int32(np),
+		PageNumber: int32(pages),
 		Object: *object,
 	}
 	return doc
