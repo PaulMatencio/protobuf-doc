0286b69a637125e4b6303a0dfc01fa74ebe4e92c
Paul Matencio <paulo.matencio@gmail.com>
2021-06-20 16:54:04 +0200

    add page size

--- lib/createPage.go
+++ lib/createPage.go
@@ -6,6 +6,7 @@ import "github.com/paulmatencio/protobuf-doc/src/document/documentpb"
 func CreatePage(pn string, metadata string, pagen int, object *[]byte) *documentpb.Page {
 	pe := &documentpb.Page{
 		PageId: pn,
+		Size : (int32)(len(*object)),
 		PageNumber: int32(pagen),
 		Metadata:   metadata,
 		Object:     *object,
